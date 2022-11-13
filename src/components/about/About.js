900ee9c9304e92a8c88e938f1c8e3024a4e319f0
Nikshay <nikshay@domain.co.za>
2022-11-13 20:04:25 +0200

    inital commit

---
+++ src/components/about/About.js
@@ -0,0 +1,13 @@
+import "./about.css";
+//import medal from "../../img/medal.png";
+
+const About = () => {
+  return (
+   <div className ="a">
+    about
+   </div>
+
+  )
+}
+
+export default About;
